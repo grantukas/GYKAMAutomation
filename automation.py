8ca66c2d5a2f9ad95bce4c4a5a17b69b7d1f2c48
Grant Stankaitis <doe123@mail.chapman.edu>
2019-05-27 20:29:12 -0500

    Update comments

--- automation.py
+++ automation.py
@@ -4,13 +4,17 @@ from selenium.webdriver.common.keys import Keys
 # Edit this block depending on Mac/Windows
 # This version is for Alena testing on Mac
 #driver = webdriver.Chrome(executable_path="/Users/alenastankaitis/Desktop/chromedriver");
-# driver = webdriver.Chrome(); # This is for use on Windows, be sure .exe is in same directory
+driver = webdriver.Chrome(); # This is for use on Windows, be sure .exe is in same directory
 
 driver.get("http://www.safeco.com")
 assert "Safeco Insurance" in driver.title
-elem = driver.find_element_by_name("location")
-elem.clear()
-elem.send_keys("60612")
-elem.send_keys(Keys.RETURN)
+location = driver.find_element_by_name("location")
+location.clear()
+location.send_keys("60612")
+location.send_keys(Keys.RETURN)
+
+contactButton = driver.find_elements_by_class_name("contactBtn")
+print(contactButton)
+
 assert "No results found." not in driver.page_source
 #driver.close()
