7890b8f6c6f59973f29f191b0511d5bf10b5e6c2
Grant Stankaitis <doe123@mail.chapman.edu>
2019-05-27 20:04:26 -0500

    Update comments

--- automation.py
+++ automation.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Edit this block depending on Mac/Windows
 # This version is for Alena testing on Mac
 #driver = webdriver.Chrome(executable_path="/Users/alenastankaitis/Desktop/chromedriver");
 # driver = webdriver.Chrome(); # This is for use on Windows, be sure .exe is in same directory
+
 driver.get("http://www.safeco.com")
 assert "Safeco Insurance" in driver.title
 elem = driver.find_element_by_name("location")
