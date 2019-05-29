940cc029042db268ddbf53ff43c22b8461e384f5
Grant Stankaitis <doe123@mail.chapman.edu>
2019-05-29 00:07:53 -0500

    Create function to click next page

--- automation.py
+++ automation.py
@@ -1,10 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+def openContacts(contacts): # Function to open each contact in a list in a new tab
+    for element in contacts:
+        if(element.text == ""):
+            continue # Skip if element is blank
+        else:
+            print(element.text) # Get element text and open each in new tab
+            ActionChains(driver) \
+                .key_down(Keys.CONTROL) \
+                .click(element) \
+                .key_up(Keys.CONTROL) \
+                .perform()
+def clickNext(): # Function to click the next button to use after searching for agencies
+    navigationList = driver.find_element_by_xpath("//div[@class='simple-pagination compact-theme']")
+    nextButton = navigationList.find_elements_by_tag_name("li")
+    for element in nextButton:
+        if (element.text == "Next"):
+            element.click()
+
 
 # Edit this block depending on Mac/Windows
 # This version is for Alena testing on Mac
 #driver = webdriver.Chrome(executable_path="/Users/alenastankaitis/Desktop/chromedriver");
-driver = webdriver.Chrome(); # This is for use on Windows, be sure .exe is in same directory
+driver = webdriver.Chrome() # This is for use on Windows, be sure .exe is in same directory
 
 driver.get("http://www.safeco.com")
 assert "Safeco Insurance" in driver.title
@@ -13,8 +33,11 @@ location.clear()
 location.send_keys("60612")
 location.send_keys(Keys.RETURN)
 
-contactButton = driver.find_elements_by_class_name("contactBtn")
-print(contactButton)
+driver.implicitly_wait(5) # Wait for page to load
 
-assert "No results found." not in driver.page_source
-#driver.close()
+# Open each contact, then click next page
+for i in range(5):
+    driver.implicitly_wait(5)
+    contactList = driver.find_elements_by_xpath("//a[contains(@href, '/find-an-insurance-agency/app/agency/')]")
+    openContacts(contactList)
+    clickNext()
